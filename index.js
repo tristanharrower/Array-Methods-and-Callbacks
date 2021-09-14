659610afd9121445c6bc1c726e2f2f6ab8af79d3
[Tristan Harrower] <[harrowertristan@gmail.com]>
2021-09-14 19:20:52 -0400

    finished all tasks

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, getFinalsCB) {
+    
+    return getFinalsCB(data).map(item => item.Year);
 }
 
 
@@ -59,9 +60,11 @@ Use the higher-order function getWinners to do the following:
 3. Determines the winner (home or away) of each `finals` game. 
 4. Returns the names of all winning countries in an array called `winners` */ 
 
-function getWinners(/* code here */) {
-    /* code here */
-    // use compare home team goals to away team goals
+function getWinners(arr,getFinalsCB) {
+    
+    return getFinalsCB(arr).map(item => item['Home Team Goals']  > item['Away Team Goals'] ?
+    item['Home Team Name'] : item['Away Team Name'])
+
 }
 
 
@@ -76,8 +79,13 @@ Use the higher-order function getWinnersByYear to do the following:
 hint: the strings returned need to exactly match the string in step 4.
  */
 
-function getWinnersByYear(/* code here */) {
-    /* code here */
+function getWinnersByYear(data, getYearsCB, getWinnersCB) {
+    
+    const winners = getWinnersCB(data, getFinals);
+
+    const years = getYearsCB(data, getFinals);
+    
+    return winners.map((item, index) => `In ${years[index]}, ${item} won the world cup!`);
 }
 
 
@@ -92,9 +100,14 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-   /* code here */
+function getAverageGoals(data) {
+   const averageHomeGoals = data.reduce(function(acc, item){
+       return acc + item['Home Team Goals'] + item['Away Team Goals'];
+   }, 0)
+
+   return (averageHomeGoals / data.length).toFixed(2);
 }
+
 
 
 
